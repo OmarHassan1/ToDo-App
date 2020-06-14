d8e597f34bc019adc09211dac79daea29f9c68a4
OmarHassin <boky555555@gmail.com>
2020-06-14 13:32:45 +0200

    AddProject file

---
+++ src/components/AddProject.js
@@ -0,0 +1,2 @@
+import React from "react";
+export const AddProject = () => <p>hello Add Projects</p>;
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,7 +7,7 @@ import {
 } from "react-icons/fa";
 import { useSelectedProjectValue } from "../../context";
 import { Projects } from "../Projects";
-
+import { AddProject } from "../AddProject";
 export const Sidebar = () => {
   const { setSelectedProject } = useSelectedProjectValue;
   const [active, setActive] = useState("inbox");
@@ -43,6 +43,7 @@ export const Sidebar = () => {
         <h2>Projects</h2>
       </div>
       <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+      {showProjects && <AddProject />}
     </div>
   );
 };
