243680cb003d9f08a0b2fb8a5a1b4c9df92dfdd8
OmarHassin <boky555555@gmail.com>
2020-06-14 21:43:00 +0200

    fix some error in Tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Checkbox } from "./Checkbox";
 import { useTasks } from "../hooks";
+
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
+
 export const Tasks = () => {
-  const { tasks } = useTasks("1");
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+  let projectName = "";
 
-  console.log(tasks);
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    console.log("projectName 1:", projectName);
+  }
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    console.log("projectName 2:", projectName);
+  }
 
-  const projectName = "";
-
+  useEffect(() => {
+    document.title = `${projectName}  ToDoApp`;
+  });
   return (
     <div className="tasks" data-testid="tasks">
-      <h2 data-testid="project-name">{projectName}</h2>
+      <h3 data-testid="project-name">{projectName}</h3>
       <ul className="tasks__list">
         {tasks.map((task) => (
           <li key={`${task.id}`}>
-            <Checkbox id={task.id} />
+            <Checkbox id={task.id} taskDesc={task.task} />
             <span>{task.task}</span>
           </li>
         ))}
