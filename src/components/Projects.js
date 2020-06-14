c26ff6d81624d1d21a2ad4db70fdcfdbd95127e9
OmarHassin <boky555555@gmail.com>
2020-06-14 13:27:53 +0200

    add Project file

---
+++ src/components/Projects.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
+import { IndividualProject } from "./individualProject";
+export const Projects = ({ activeValue = null }) => {
+  const [active, setActive] = useState(activeValue);
+  const { setSelectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  return (
+    projects &&
+    projects.map((project) => (
+      <li
+        key={project.projectid}
+        data-doc-id={project.docId}
+        data-testid="project-action-parent"
+        className={
+          active === project.projectid
+            ? "active sidebar__project"
+            : "sidebar__project"
+        }
+        onClick={() => {
+          setActive(project.projectid);
+          setSelectedProject(project.projectid);
+        }}
+        onKeyDown={() => {
+          setActive(project.projectid);
+          setSelectedProject(project.projectid);
+        }}
+      >
+        <IndividualProject project={project} />
+      </li>
+    ))
+  );
+};
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar,
 } from "react-icons/fa";
-export const Sidebar = () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-      <li data-testid="inbox" className="inbox">
-        <span>
-          <FaInbox />
-        </span>
-        <span>Inbox</span>
-      </li>
+import { useSelectedProjectValue } from "../../context";
+import { Projects } from "../Projects";
 
-      <li data-testid="today" className="today">
-        <span>
-          <FaRegCalendar />
-        </span>
-        <span>Today</span>
-      </li>
+export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li data-testid="inbox" className="inbox">
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
 
-      <li data-testid="next_7" className="next_7">
+        <li data-testid="today" className="today">
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+
+        <li data-testid="next_7" className="next_7">
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 Days</span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
         <span>
-          <FaRegCalendarAlt />
+          <FaChevronDown />
         </span>
-        <span>Next 7 Days</span>
-      </li>
-    </ul>
-    <div className="sidebar__middle">
-      <span>
-        <FaChevronDown />
-      </span>
-      <h2>Projects</h2>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
     </div>
-    <ul className="sidebar__projects">Projects will be here</ul>
-  </div>
-);
+  );
+};
